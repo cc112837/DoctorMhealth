914e37984178149e0cef786d56346b7340a3217c
cc <cc112837@163.com>
2016-10-28 15:25:58 +0800

    我的评价页面的实现

--- app/src/main/java/com/cc/doctormhealth/activity/PrideActivity.java
+++ app/src/main/java/com/cc/doctormhealth/activity/PrideActivity.java
@@ -1,14 +1,27 @@
 package com.cc.doctormhealth.activity;
 
 import android.app.Activity;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.cc.doctormhealth.R;
+import com.cc.doctormhealth.adapter.EvaluationListAdapter;
 
 public class PrideActivity extends Activity implements View.OnClickListener {
     private ImageView leftBtn;
+    private ListView lv_show, cityListView, locationListView;
+    private PopupWindow mPopupWindow;
+    private EvaluationListAdapter evaluationListAdapter;
+    private LinearLayout total_location, layout_left;
+    private TextView text_address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,48 @@ public class PrideActivity extends Activity implements View.OnClickListener {
 
     private void init() {
         leftBtn = (ImageView) findViewById(R.id.leftBtn);
+        lv_show = (ListView) findViewById(R.id.lv_show);
+        evaluationListAdapter = new EvaluationListAdapter(PrideActivity.this, null);
+        lv_show.setAdapter(evaluationListAdapter);
+
+    }
+
+    private void showPopupWindow(int width, int height) {
+        layout_left = (LinearLayout) LayoutInflater.from(
+                PrideActivity.this).inflate(R.layout.popup_category, null);
+        locationListView = (ListView) layout_left
+                .findViewById(R.id.rootcategory);
+        cityListView = (ListView) layout_left.findViewById(R.id.childcategory);
+        cityListView.setVisibility(View.INVISIBLE);
+        locationListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                cityListView.setVisibility(View.VISIBLE);
+                cityListView.setAdapter(null);
+            }
+        });
+        cityListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mPopupWindow.dismiss();
+
+                text_address.setText("全部");
+            }
+        });
+        mPopupWindow = new PopupWindow(layout_left, width, height * 2 / 3, true);
+        mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                text_address = (TextView) total_location.findViewById(R.id.text_address);
+                text_address.setTextColor(0xff000000);
+            }
+        });
+        mPopupWindow.setBackgroundDrawable(new BitmapDrawable());
+        mPopupWindow.showAsDropDown(total_location, 5, 1);
+        mPopupWindow.update();
     }
 
     @Override
---
+++ app/src/main/java/com/cc/doctormhealth/adapter/EvaluationListAdapter.java
@@ -0,0 +1,76 @@
+package com.cc.doctormhealth.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.cc.doctormhealth.R;
+import com.cc.doctormhealth.model.UserEvaluation;
+
+import java.util.List;
+
+public class EvaluationListAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<UserEvaluation> list;
+    private LayoutInflater mInflater;
+
+    public EvaluationListAdapter(Context context, List<UserEvaluation> list) {
+        this.context = context;
+        this.list = list;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        ViewHolder vholder = null;
+        if (view == null) {
+            vholder = new ViewHolder();
+            view = mInflater.inflate(R.layout.item_evaluation_list, null);
+            vholder.tv_data = (TextView) view.findViewById(R.id.tv_data);
+            vholder.tv_month = (TextView) view.findViewById(R.id.tv_month);
+            vholder.name = (TextView) view.findViewById(R.id.yonghu);
+            vholder.degree = (TextView) view.findViewById(R.id.yonghuDegree);
+            vholder.pingjia = (TextView) view.findViewById(R.id.yonghupingjia);
+            view.setTag(vholder);
+        } else {
+            vholder = (ViewHolder) view.getTag();
+        }
+        String str = list.get(position).getUserid();
+        vholder.name.setText("用户："+str.charAt(0) + "****"
+                + str.charAt(str.length() - 3));
+        vholder.tv_data.setText("");
+        vholder.tv_month.setText("");
+        vholder.degree.setText("满意度："+list.get(position).getDegree() + "");
+        vholder.pingjia.setText(list.get(position).getComment());
+        return view;
+
+    }
+
+    static class ViewHolder {
+        public TextView name;
+        public TextView degree;
+        public TextView pingjia;
+        public TextView tv_month;
+        public TextView tv_data;
+    }
+
+}
