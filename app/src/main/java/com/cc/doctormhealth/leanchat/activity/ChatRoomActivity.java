a9a1a5b7a23c9fba7cb58986c1b4901c9c6ebcbf
cc <cc112837@163.com>
2017-01-16 10:48:32 +0800

    添加聊天返回按钮

--- app/src/main/java/com/cc/doctormhealth/leanchat/activity/ChatRoomActivity.java
+++ app/src/main/java/com/cc/doctormhealth/leanchat/activity/ChatRoomActivity.java
@@ -2,16 +2,11 @@ package com.cc.doctormhealth.leanchat.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import com.avos.avoscloud.AVGeoPoint;
-import com.avos.avoscloud.im.v2.messages.AVIMLocationMessage;
 import com.avoscloud.leanchatlib.activity.AVChatActivity;
-import com.avoscloud.leanchatlib.event.InputBottomBarLocationClickEvent;
-import com.avoscloud.leanchatlib.event.LocationItemClickEvent;
 import com.avoscloud.leanchatlib.utils.Constants;
 import com.avoscloud.leanchatlib.utils.NotificationUtils;
 import com.cc.doctormhealth.R;
@@ -20,38 +15,37 @@ import com.cc.doctormhealth.R;
  * Created by cc112837@163.com on 16/5/18.
  */
 public class ChatRoomActivity extends AVChatActivity {
-	public static final int LOCATION_REQUEST = 100;
-	public static final int QUIT_GROUP_REQUEST = 200;
+    public static final int QUIT_GROUP_REQUEST = 200;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-	@Override
-	protected void onResume() {
-		NotificationUtils.cancelNotification(this);
-		super.onResume();
-	}
+    @Override
+    protected void onResume() {
+        NotificationUtils.cancelNotification(this);
+        super.onResume();
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.chat_ativity_menu, menu);
-		alwaysShowMenuItem(menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.chat_ativity_menu, menu);
+        alwaysShowMenuItem(menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int menuId = item.getItemId();
-		if (menuId == R.id.people) {
-			if (null != conversation) {
-				Intent intent = new Intent(ChatRoomActivity.this, ConversationDetailActivity.class);
-				intent.putExtra(Constants.CONVERSATION_ID, conversation.getConversationId());
-				startActivityForResult(intent, QUIT_GROUP_REQUEST);
-			}
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuId = item.getItemId();
+        if (menuId == R.id.people) {
+            if (null != conversation) {
+                Intent intent = new Intent(ChatRoomActivity.this, ConversationDetailActivity.class);
+                intent.putExtra(Constants.CONVERSATION_ID, conversation.getConversationId());
+                startActivityForResult(intent, QUIT_GROUP_REQUEST);
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- leanchatlib/src/main/java/com/avoscloud/leanchatlib/activity/ImageBrowserActivity.java
+++ leanchatlib/src/main/java/com/avoscloud/leanchatlib/activity/ImageBrowserActivity.java
@@ -1,36 +1,37 @@
-package com.cc.doctormhealth.leanchat.activity;
+package com.avoscloud.leanchatlib.activity;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+
 import com.avoscloud.leanchatlib.R;
+import com.avoscloud.leanchatlib.utils.Constants;
 import com.avoscloud.leanchatlib.utils.PhotoUtils;
 
 /**
  * Created by lzw on 14-9-21.
  */
 public class ImageBrowserActivity extends Activity {
-  private static final String PATH = "path";
-  private static final String URL = "url";
-  private ImageView imageView;
 
-  public static void go(Context ctx, String path, String url) {
-    Intent intent = new Intent(ctx, ImageBrowserActivity.class);
-    intent.putExtra(PATH, path);
-    intent.putExtra(URL, url);
-    ctx.startActivity(intent);
-  }
+  private ImageView imageView,leftBtn;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.chat_image_brower_layout);
     imageView = (ImageView) findViewById(R.id.imageView);
+    leftBtn=(ImageView) findViewById(R.id.leftBtn);
+    leftBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        finish();
+      }
+    });
     Intent intent = getIntent();
-    String path = intent.getStringExtra(PATH);
-    String url = intent.getStringExtra(URL);
+    String path = intent.getStringExtra(Constants.IMAGE_LOCAL_PATH);
+    String url = intent.getStringExtra(Constants.IMAGE_URL);
     PhotoUtils.displayImageCacheElseNetwork(imageView, path, url);
   }
 }
